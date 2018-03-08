87546c70cc06f47f8eecc39d1619d51dbe6f38ee
carlos <kenshinmc23@gmail.com>
2018-03-08 17:35:11 -0400

    2.- generar componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { UserListComponent } from './user-list/user-list.component';
+import { UserFormEditComponent } from './user-form-edit/user-form-edit.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    UserListComponent,
+    UserFormEditComponent
   ],
   imports: [
     BrowserModule,
